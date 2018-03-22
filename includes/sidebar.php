6d172de633dcbc2f9fc8dc6fd05572869e97d571
pragyatak <pragyatak@csu.fullerton.edu>
2018-03-22 09:51:17 -0700

    Add files via upload

---
+++ includes/sidebar.php
@@ -0,0 +1,69 @@
+<aside class="col-md-4 blog-sidebar">
+  <div class="p-3">
+    <h4 class="font-italic">Search</h4>
+
+    <form method="get" action="results.php" class="form-inline">
+
+     <div class="input-group mb-2 mr-sm-2">
+         <input type="text" name="search" class="form-control mb-2 mr-sm-2" id="inlineFormInputName2" placeholder="Search...">
+     </div>
+
+   </form>
+  </div>
+
+  <div class="p-3 mb-3 bg-light rounded">
+    <h4 class="font-italic">About</h4>
+    <p class="mb-0">Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
+  </div>
+
+  <div class="p-3">
+
+    <h4 class="font-italic">Subscribe</h4>
+    <form method="post">
+       <div class="form-group row">
+         <div class="col-sm-10">
+           <input type="text" name="name" class="form-control" id="inputEmail3" placeholder="Name">
+         </div>
+       </div>
+       <div class="form-group row">
+         <div class="col-sm-10">
+           <input type="email" name="email" class="form-control" id="inputEmail3" placeholder="Email">
+         </div>
+       </div>
+
+       <div class="form-group row">
+         <div class="col-sm-10">
+           <button type="submit" class="btn btn-primary">Subscribe</button>
+         </div>
+       </div>
+     </form>
+
+  </div>
+
+  <div class="p-3">
+    <h4 class="font-italic">Archives</h4>
+    <ol class="list-unstyled mb-0">
+      <li><a href="#">March 2014</a></li>
+      <li><a href="#">February 2014</a></li>
+      <li><a href="#">January 2014</a></li>
+      <li><a href="#">December 2013</a></li>
+      <li><a href="#">November 2013</a></li>
+      <li><a href="#">October 2013</a></li>
+      <li><a href="#">September 2013</a></li>
+      <li><a href="#">August 2013</a></li>
+      <li><a href="#">July 2013</a></li>
+      <li><a href="#">June 2013</a></li>
+      <li><a href="#">May 2013</a></li>
+      <li><a href="#">April 2013</a></li>
+    </ol>
+  </div>
+
+  <div class="p-3">
+    <h4 class="font-italic">Elsewhere</h4>
+    <ol class="list-unstyled">
+      <li><a href="#">GitHub</a></li>
+      <li><a href="#">Twitter</a></li>
+      <li><a href="#">Facebook</a></li>
+    </ol>
+  </div>
+</aside><!-- /.blog-sidebar -->
